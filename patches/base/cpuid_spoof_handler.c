294764ab40ac518acf82debe1760b5570d9bbe06
brcly <barclay@tuta.io>
2026-07-31 12:10:35 +0100

    main: sync cpuid_spoof_handler with marker + spoof_* locals

--- patches/base/cpuid_spoof_handler.c
+++ patches/base/cpuid_spoof_handler.c
@@ -1,99 +1,103 @@
-    unsigned int leaf;
-    unsigned int subleaf;
-    ucontext_t *uc;
-    unsigned char *rip;
+    /* linuwux-cpuid-handler */
+    {
+        unsigned int spoof_leaf;
+        unsigned int spoof_subleaf;
+        ucontext_t *spoof_uc;
+        unsigned char *spoof_rip;
 
-    uc = (ucontext_t *)sigcontext;
-    rip = (unsigned char *)uc->uc_mcontext.gregs[REG_RIP];
-    leaf = ucontext->uc_mcontext.gregs[REG_RAX];
-    subleaf = ucontext->uc_mcontext.gregs[REG_RCX];
-    if ((siginfo->si_code == SI_KERNEL || leaf == 0x336933) && rip[0] == 0x0F && rip[1] == 0xA2) {
-        // Spoof CPUID results based on leaf
-        switch (leaf) {
-            case 1:
-                uc->uc_mcontext.gregs[REG_RAX] = spoof_leaf1_eax;
-                uc->uc_mcontext.gregs[REG_RBX] = spoof_leaf1_ebx;
-                uc->uc_mcontext.gregs[REG_RCX] = spoof_leaf1_ecx | (TargetSysHandler ? 0 : (0x1 << 31));
-                uc->uc_mcontext.gregs[REG_RDX] = spoof_leaf1_edx;
-                break;
+        spoof_uc = (ucontext_t *)sigcontext;
+        spoof_rip = (unsigned char *)spoof_uc->uc_mcontext.gregs[REG_RIP];
+        /* leaf/subleaf from the init_handler-initialized ucontext */
+        spoof_leaf = ucontext->uc_mcontext.gregs[REG_RAX];
+        spoof_subleaf = ucontext->uc_mcontext.gregs[REG_RCX];
+        if ((siginfo->si_code == SI_KERNEL || spoof_leaf == 0x336933) && spoof_rip[0] == 0x0F && spoof_rip[1] == 0xA2) {
+            // Spoof CPUID results based on leaf
+            switch (spoof_leaf) {
+                case 1:
+                    spoof_uc->uc_mcontext.gregs[REG_RAX] = spoof_leaf1_eax;
+                    spoof_uc->uc_mcontext.gregs[REG_RBX] = spoof_leaf1_ebx;
+                    spoof_uc->uc_mcontext.gregs[REG_RCX] = spoof_leaf1_ecx | (TargetSysHandler ? 0 : (0x1 << 31));
+                    spoof_uc->uc_mcontext.gregs[REG_RDX] = spoof_leaf1_edx;
+                    break;
 
-            case 0x40000000:
-                uc->uc_mcontext.gregs[REG_RAX] = spoof_leaf40000000_eax;
-                uc->uc_mcontext.gregs[REG_RBX] = spoof_leaf40000000_ebx;
-                uc->uc_mcontext.gregs[REG_RCX] = spoof_leaf40000000_ecx;
-                uc->uc_mcontext.gregs[REG_RDX] = spoof_leaf40000000_edx;
-                break;
+                case 0x40000000:
+                    spoof_uc->uc_mcontext.gregs[REG_RAX] = spoof_leaf40000000_eax;
+                    spoof_uc->uc_mcontext.gregs[REG_RBX] = spoof_leaf40000000_ebx;
+                    spoof_uc->uc_mcontext.gregs[REG_RCX] = spoof_leaf40000000_ecx;
+                    spoof_uc->uc_mcontext.gregs[REG_RDX] = spoof_leaf40000000_edx;
+                    break;
 
-            case 0x40000001:
-                uc->uc_mcontext.gregs[REG_RAX] = spoof_leaf40000001_eax;
-                uc->uc_mcontext.gregs[REG_RBX] = spoof_leaf40000001_ebx;
-                uc->uc_mcontext.gregs[REG_RCX] = spoof_leaf40000001_ecx;
-                uc->uc_mcontext.gregs[REG_RDX] = spoof_leaf40000001_edx;
-                break;
+                case 0x40000001:
+                    spoof_uc->uc_mcontext.gregs[REG_RAX] = spoof_leaf40000001_eax;
+                    spoof_uc->uc_mcontext.gregs[REG_RBX] = spoof_leaf40000001_ebx;
+                    spoof_uc->uc_mcontext.gregs[REG_RCX] = spoof_leaf40000001_ecx;
+                    spoof_uc->uc_mcontext.gregs[REG_RDX] = spoof_leaf40000001_edx;
+                    break;
 
-            case 0x80000002:
-                uc->uc_mcontext.gregs[REG_RAX] = 0x756E6544;
-                uc->uc_mcontext.gregs[REG_RBX] = 0x4F774F76;
-                uc->uc_mcontext.gregs[REG_RCX] = 0x55504320;
-                uc->uc_mcontext.gregs[REG_RDX] = 0x31204020;
-                break;
+                case 0x80000002:
+                    spoof_uc->uc_mcontext.gregs[REG_RAX] = 0x756E6544;
+                    spoof_uc->uc_mcontext.gregs[REG_RBX] = 0x4F774F76;
+                    spoof_uc->uc_mcontext.gregs[REG_RCX] = 0x55504320;
+                    spoof_uc->uc_mcontext.gregs[REG_RDX] = 0x31204020;
+                    break;
 
-            case 0x80000003:
-                uc->uc_mcontext.gregs[REG_RAX] = 0x20373333;
-                uc->uc_mcontext.gregs[REG_RBX] = 0x007A4847;
-                uc->uc_mcontext.gregs[REG_RCX] = 0x00000000;
-                uc->uc_mcontext.gregs[REG_RDX] = 0x00000000;
-                break;
+                case 0x80000003:
+                    spoof_uc->uc_mcontext.gregs[REG_RAX] = 0x20373333;
+                    spoof_uc->uc_mcontext.gregs[REG_RBX] = 0x007A4847;
+                    spoof_uc->uc_mcontext.gregs[REG_RCX] = 0x00000000;
+                    spoof_uc->uc_mcontext.gregs[REG_RDX] = 0x00000000;
+                    break;
 
-            case 0x80000004:
-                uc->uc_mcontext.gregs[REG_RAX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RBX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RCX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RDX] = 0x0;
-                break;
+                case 0x80000004:
+                    spoof_uc->uc_mcontext.gregs[REG_RAX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RBX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RCX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RDX] = 0x0;
+                    break;
 
-            case 0x336933:
-                MESSAGE("Spoofing CPUID leaf %x\n", leaf);
-                TargetSysHandler = uc->uc_mcontext.gregs[REG_RCX];
-                patch_kuser_shared_data();
-                uc->uc_mcontext.gregs[REG_RAX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RBX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RCX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RDX] = 0x0;
-                break;
+                case 0x336933:
+                    MESSAGE("Spoofing CPUID leaf %x\n", spoof_leaf);
+                    TargetSysHandler = spoof_uc->uc_mcontext.gregs[REG_RCX];
+                    patch_kuser_shared_data();
+                    spoof_uc->uc_mcontext.gregs[REG_RAX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RBX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RCX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RDX] = 0x0;
+                    break;
 
-            case 0x336967:
-                MESSAGE("Setting Faketime to %llx... \n", uc->uc_mcontext.gregs[REG_RCX]);
-                SERVER_START_REQ( set_faketime )
-                {
-                    req->faketime = uc->uc_mcontext.gregs[REG_RCX];
-                    wine_server_call( req );
-                }
-                SERVER_END_REQ;
-                uc->uc_mcontext.gregs[REG_RAX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RBX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RCX] = 0x0;
-                uc->uc_mcontext.gregs[REG_RDX] = 0x0;
-                break;
+                case 0x336967:
+                    MESSAGE("Setting Faketime to %llx... \n", spoof_uc->uc_mcontext.gregs[REG_RCX]);
+                    SERVER_START_REQ( set_faketime )
+                    {
+                        req->faketime = spoof_uc->uc_mcontext.gregs[REG_RCX];
+                        wine_server_call( req );
+                    }
+                    SERVER_END_REQ;
+                    spoof_uc->uc_mcontext.gregs[REG_RAX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RBX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RCX] = 0x0;
+                    spoof_uc->uc_mcontext.gregs[REG_RDX] = 0x0;
+                    break;
 
-            default:
-                // Should implement caching for optimization
-                // Disable CPUID faulting for real CPUID call
-                syscall(SYS_arch_prctl, ARCH_SET_CPUID, 1);
-                __asm__ volatile(
-                        "cpuid"
-                        : "=a"(uc->uc_mcontext.gregs[REG_RAX]),
-                          "=b"(uc->uc_mcontext.gregs[REG_RBX]),
-                          "=c"(uc->uc_mcontext.gregs[REG_RCX]),
-                          "=d"(uc->uc_mcontext.gregs[REG_RDX])
-                        : "a"(leaf), "c"(subleaf)
-                        : "memory"
-                    );
-                // Enable CPUID faulting again
-                syscall(SYS_arch_prctl, ARCH_SET_CPUID, 0);
+                default:
+                    // Should implement caching for optimization
+                    // Disable CPUID faulting for real CPUID call
+                    syscall(SYS_arch_prctl, ARCH_SET_CPUID, 1);
+                    __asm__ volatile(
+                            "cpuid"
+                            : "=a"(spoof_uc->uc_mcontext.gregs[REG_RAX]),
+                              "=b"(spoof_uc->uc_mcontext.gregs[REG_RBX]),
+                              "=c"(spoof_uc->uc_mcontext.gregs[REG_RCX]),
+                              "=d"(spoof_uc->uc_mcontext.gregs[REG_RDX])
+                            : "a"(spoof_leaf), "c"(spoof_subleaf)
+                            : "memory"
+                        );
+                    // Enable CPUID faulting again
+                    syscall(SYS_arch_prctl, ARCH_SET_CPUID, 0);
+            }
+
+            // Skip the CPUID instruction (2 bytes: 0F A2)
+            spoof_uc->uc_mcontext.gregs[REG_RIP] += 2;
+            return;
         }
-
-        // Skip the CPUID instruction (2 bytes: 0F A2)
-        uc->uc_mcontext.gregs[REG_RIP] += 2;
-        return;
     }
