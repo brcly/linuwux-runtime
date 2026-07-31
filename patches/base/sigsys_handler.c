cdd6916101e003fa341760b169c6765402d78d98
brcly <barclay@tuta.io>
2026-07-31 20:42:28 +0100

    sigsys: call stub for linuwux_sigsys_route

--- patches/base/sigsys_handler.c
+++ patches/base/sigsys_handler.c
@@ -1,13 +1,3 @@
-    /* linuwux-sigsys-handler */
-    ucontext_t *ctx = sigcontext;
-    __uint128_t *xmm_regs = (__uint128_t *)ctx->uc_mcontext.fpregs->_xmm;
-    if (TargetSysHandler != 0 && (xmm_regs[5] & 0xFFFFFFFFFFFFFFFF) != 0x1337133713371337) {
-        xmm_regs[4] = ctx->uc_mcontext.gregs[REG_RAX] & 0xFFFFFFFF;
-        ctx->uc_mcontext.gregs[REG_RAX] = ctx->uc_mcontext.gregs[REG_RCX];
-        ctx->uc_mcontext.gregs[REG_RCX] = TargetSysHandler;
-        ctx->uc_mcontext.gregs[REG_RIP] = TargetSysHandler;
+    /* linuwux-sigsys-handler-call */
+    if (linuwux_sigsys_route(sigcontext))
         return;
-    }
-    if ((xmm_regs[5] & 0xFFFFFFFFFFFFFFFF) == 0x1337133713371337) {
-        xmm_regs[5] = 0;
-    }
